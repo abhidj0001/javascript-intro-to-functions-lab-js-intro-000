5bb199d4f55fd5e53f424999f287f297813abea0
Abhishek Jadhav <abhidj0001@gmail.com>
2019-08-18 04:52:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,13 +18,12 @@ function sayHiToGrandma(string) {
 if(string.toLowerCase()===string){
   return "I can't hear you!"
 }
-}
 
-function sayHiToGrandma(string) {
 if(string.toUpperCase()===string){
   return "YES INDEED!"
 }
-}
 
-if(string==="I love you, Grandma.")
+if(string==="I love you, Grandma."){
 return "I love you too."
+}
+}
