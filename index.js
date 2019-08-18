6e7c1e4326f25902803bb95b60cabcfac5d90045
Abhishek Jadhav <abhidj0001@gmail.com>
2019-08-18 04:50:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function logWhisper(string){
 function logShout(string){
   console.log(string.toUpperCase())
 }
+
+function sayHiToGrandma(string) {
+if(string.toLowerCase()===string){
+  return "I can't hear you!"
+}  
+}
+
+function sayHiToGrandma(string) {
+if(string.toUpperCase()===string){
+  return "YES INDEED!"
+}  
+}
+
+if(string==="I love you, Grandma.")
+return "I love you too."
